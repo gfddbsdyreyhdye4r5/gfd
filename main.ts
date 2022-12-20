4634c6971bfaad5718701a2752081682e261ba37
gfddbsdyreyhdye4r5 <97019395+gfddbsdyreyhdye4r5@users.noreply.github.com>
2022-12-20 11:27:33 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    d = randint(1, 100)
+    if (d <= 60) {
+        basic.showString("C")
+    }
+    if (d >= 61) {
+        basic.showString("B")
+    }
+    if (d >= 91) {
+        basic.showString("A")
+    }
+})
+input.onGesture(Gesture.LogoUp, function () {
+	
+})
+let d = 0
+d = 0
+basic.forever(function () {
+	
+})
